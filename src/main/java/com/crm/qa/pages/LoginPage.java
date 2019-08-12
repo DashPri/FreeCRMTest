c55e498aae9eaacd520424ccd6962c5adb32e237
Pritam Dash <dashpri@ad.issgovernance.com>
2019-08-12 16:53:18 +0530

    changes in Login Page

--- src/main/java/com/crm/qa/pages/LoginPage.java
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -3,6 +3,7 @@ package com.crm.qa.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.crm.qa.base.TestBase;
@@ -11,7 +12,7 @@ public class LoginPage extends TestBase {
 
 	//PageFactory-OR(Object Repository:
 	
-	/*@FindBy(name="username")
+	@FindBy(name="username")
 	WebElement username;
 	
 	@FindBy(name="password")
@@ -24,13 +25,13 @@ public class LoginPage extends TestBase {
 	WebElement signUpBtn;
 	
 	@FindBy(xpath= "//img[contains(@class,'img-responsive')]")
-	WebElement crmLogo ;*/
+	WebElement crmLogo ;
 	
-	WebElement username = driver.findElement(By.name("username"));
+/*	WebElement username = driver.findElement(By.name("username"));
 	WebElement password = driver.findElement(By.name("password"));
 	WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
 	WebElement signUpBtn = driver.findElement(By.xpath("//button[contains(text(),'Sign Up')]"));
-	WebElement crmLogo = driver.findElement(By.xpath("//img[contains(@class,'img-responsive')]"));
+	WebElement crmLogo = driver.findElement(By.xpath("//img[contains(@class,'img-responsive')]"));*/
 	
 	public LoginPage(){
 		PageFactory.initElements(driver, this);
